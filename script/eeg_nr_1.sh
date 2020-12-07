c2949c5c865e3372719128dcb1b10d075ec04534
César Rocha <rhcesar95@gmail.com>
2020-12-07 04:49:25 -0600

    modified: Execution scripts modified

--- script/eeg_nr_1.sh
+++ script/eeg_nr_1.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+module load Python
+
+export WORKON_HOME=$HOME/.virtualenvs
+export VIRTUALENVWRAPPER_PYTHON=/usr/local/Cluster-software/software/Python/3.8.6-GCCcore-10.2.0/bin/python3
+export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/Cluster-software/software/Python/3.8.6-GCCcore-10.2.0/bin/virtualenv
+source /usr/local/Cluster-software/software/Python/3.8.6-GCCcore-10.2.0/bin/virtualenvwrapper.sh
+
+workon evobcienv
+
 #$ -t 1-31
 #$ -N eeg_nr_1
 #$ -cwd
