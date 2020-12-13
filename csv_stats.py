ee61ba7b5a2bd4ea4a0d6c4c342be73281d2f3cc
César Rocha <rhcesar95@gmail.com>
2020-12-13 06:09:21 -0600

    add: csv_stats.py was added

---
+++ csv_stats.py
@@ -0,0 +1,62 @@
+import click
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from datetime import timedelta
+
+wsize = 250
+channels = ['Fp1', 'Fp2', 'F3', 'F4', 'P1', 'P2', 'O1', 'O2']
+
+def load_data(inputpath):
+	# Generate a list of Path objects by searching CSV files
+	path = Path(inputpath)
+	filepath = list(path.glob('*.csv'))
+	# Iterate through each path in list and open it as a DataFrame
+	file_id = list()
+	elapsed_time = list()
+	experiment = pd.DataFrame()
+	for path in filepath:
+		df = pd.read_csv(path)
+		gbest = df.tail(1)
+		file_number = path.stem.split('_')[1]
+		file_id.append(int(file_number))
+		seconds = df['elapsed'].sum()
+		elapsed_time.append(str(timedelta(seconds=seconds)))
+		experiment = experiment.append(gbest, ignore_index=True)
+	experiment.insert(0, 'file_id', file_id)
+	experiment['elapsed'] = elapsed_time
+	experiment.sort_values(by='fitness', inplace=True, ignore_index=True)
+	return experiment
+
+def compute_stats(kwargs):
+	experiment = load_data(kwargs['inputpath'])
+	# Summary of channel selection
+	for name in channels:
+		positions = [f'{name}_{i + 1}' for i in range(wsize)]
+		experiment[name] = experiment[positions].sum(axis=1) * experiment[name]
+		experiment.drop(columns=positions, inplace=True)
+	total_points = experiment[channels].sum(axis=1)
+	pos = int(np.where(experiment.columns == 'TP')[0][0])
+	experiment.insert(pos, 'points', total_points)
+	cols_to_int = experiment.columns[2:-2]
+	experiment[cols_to_int] = experiment[cols_to_int].applymap(int)
+	# Verbose mode
+	if kwargs['v']:
+		print('# Summary')
+		print(experiment)
+	# Compute mean, std, median
+	stats = pd.Series(dtype='float64')
+	stats['std'] = experiment['fitness'].std()
+	stats['mean'] = experiment['fitness'].mean()
+	stats['median'] = experiment['fitness'].median()
+	print('# Statistics')
+	print(stats)
+
+@click.command()
+@click.argument('INPUTPATH', type=click.Path(exists=True, file_okay=False), required=True)
+@click.option('-v', is_flag=True, default=False, show_default=True, help='Verbose mode. Shows a summary of executions')
+def main(**kwargs):
+	compute_stats(kwargs)
+
+if __name__ == '__main__':
+	main()
